9ef7ad3c7161ad5cb65336dd5aec9784d13d522b
dedechndra <73011460+dedechndra@users.noreply.github.com>
2020-10-31 16:27:58 +0800

    Upload

---
+++ ServerdanClient_DedeChandra_20180140106/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_DedeChandra_20180140106
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
